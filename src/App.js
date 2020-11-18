af5b137ab2721a14969b9c709936c84420f273f0
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-11-18 11:17:48 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -32,20 +32,30 @@ handleOnClick = () => {
   this.setState( newState )
 }
 
+
+backToNormal = () => this.setState({ num: 12, input: '', passwordGenerated: false })
+
   render(){
-  if (this.state.passwordGenerated) return <div className="App"><br></br><h2>{this.passwordGenerator(parseInt(this.state.num))}</h2></div>
+  if (this.state.passwordGenerated) 
+    return (
+    <div className="App">
+      <br></br>
+      <h2>{this.passwordGenerator(parseInt(this.state.num))}</h2>
+      <br></br>
+      <button className='ui button' onClick={this.backToNormal}>Another Password</button>
+    </div>
+    )
     return (
   <div className="App">
     <br></br>
     <h3>How Long Does your Password Need to be?</h3>
     <br></br>
     <div className='ui input'>
-      <input placeholder='Number...' value={this.state.input} onChange={this.handleOnChange}></input>
+      <input placeholder='12 digits...' value={this.state.input} onChange={this.handleOnChange}></input>
     </div>
     <br></br>
     <br></br>
-    <button className='ui button' onClick={this.handleOnClick}>Submit</button>
-    
+    <button className='ui button' onClick={this.handleOnClick}>Give Me A Password</button>
   </div>
 )
   }
