b5ee9dab3658b042a0d33e58350d4adca5b0de4e
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-11-18 10:48:16 -0500

    aditing

--- src/App.js
+++ src/App.js
@@ -11,9 +11,8 @@ class App extends Component {
   passwordGenerator = (passwordNeededLength=12) => {
     const characters = ['a','b','c','d','e','f','g','!' ,'@','$','%','k','l', 'm', 'n','o','p','2','4','6','7','8','9', 'Q', 'R', 'S','T','U','V','W','X','Y', 'Z']
     let string = ''
-    let length = passwordNeededLength
     let count = 0 
-    while (count < length){
+    while (count < passwordNeededLength){
         let randomChar = Math.floor(Math.random() * characters.length)
         string = string + characters[randomChar]
         count ++ 
