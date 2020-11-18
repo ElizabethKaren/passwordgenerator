7b23e36682ecb1fd05d8ea96bed24d8bb9b52439
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-11-18 10:48:42 -0500

    editing

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     passwordGenerated: false 
   }
 
-  passwordGenerator = (passwordNeededLength=12) => {
+  passwordGenerator = (passwordNeededLength) => {
     const characters = ['a','b','c','d','e','f','g','!' ,'@','$','%','k','l', 'm', 'n','o','p','2','4','6','7','8','9', 'Q', 'R', 'S','T','U','V','W','X','Y', 'Z']
     let string = ''
     let count = 0 
