a99832bde394b16283ecb61bc1151fbaf5f46538
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-11-18 10:52:37 -0500

    colorUpdated

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import 'semantic-ui-css/semantic.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
+    <div style={{ backgroundColor: "dodgerblue", height: '100vh' }}>
     <App />
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
